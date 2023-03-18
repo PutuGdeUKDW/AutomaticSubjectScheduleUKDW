295948c44f4b57098cbb71599df3169bc4cd7c04
PutuGdeUKDW <93354513+PutuGdeUKDW@users.noreply.github.com>
2023-03-18 13:55:50 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,11 +16,11 @@ for i in range(7):
 print(title_card)
 
 krs_raw = {
-'senin': [['KaKom-C',''],['RPLBO-A','RPLBO-B',''],['PROGWEB-C','PROGWEB-D',''],['']],
-'selasa':[['PrRPLBO-A1','PrRPLBO-B1','AI-C','AI-D',''],['RPLBO-C','RPLBO-D','AI-A','AI-B',''],['PROGWEB-A','PROGWEB-B','EtProf-C','EtProf-D',''],['']],
-'rabu':[['PrRPLBO-C1','PrRPLBO-D1','KaKom-B',''],['PrPROGWEB-A','PrPROGWEB-B',''],['PP-H','PP-I','PrPROGWEB-C','PrPROGWEB-D',''],['']],
-'kamis':[['KaKom-A','KaKom-D',''],['PrRPLBO-A2','PrRPLBO-B2',''],['PP-J','PP-K','EtProf-B',''],['']],
-'jumat':[['PrRPLBO-C2','PrRPLBO-D2',''],[''],[''],['']]
+'senin': [[''],[''],[''],['']],
+'selasa':[[''],[''],[''],['']],
+'rabu':[[''],[''],[''],['']],
+'kamis':[[''],[''],[''],['']],
+'jumat':[[''],[''],[''],['']]
     }
 
 
